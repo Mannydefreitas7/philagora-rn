4bb1adae716195372095f10480afb3f670442dc2
Emmanuel De Freitas <manny.defreitas7@me.com>
2026-02-23 21:21:00 -0800

    Add useColor hook for dark/light themes

---
+++ apps/mobile/hooks/use-color.ts
@@ -0,0 +1,12 @@
+import { oklabToHex } from "@/utils/convert";
+import { useMemo } from "react";
+import colors from "tailwindcss/colors";
+
+export const useColor = (isDark: boolean) => {
+  const activeColorToken = useMemo(() => (isDark ? colors.cyan[300] : colors.cyan[800]), [isDark]);
+  const colorToken = useMemo(() => (isDark ? colors.neutral[600] : colors.neutral[400]), [isDark]);
+  const activeColor = useMemo(() => oklabToHex(activeColorToken), [activeColorToken]);
+  const color = useMemo(() => oklabToHex(colorToken), [colorToken]);
+
+  return { activeColor, color };
+};
