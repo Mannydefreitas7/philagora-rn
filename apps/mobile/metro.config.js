d41c0957c2a33384f4ff8653d155cb1ff61fb72f
Emmanuel De Freitas <manny.defreitas7@me.com>
2026-02-28 19:03:41 -0800

    Update mobile Metro config and deps
    - Configure Metro to use the SVG transformer
    - Register SVG as a source ext for assets and SVGs
    - Bump Expo SDK to 55 and React Native to 0.83.2
    - Update related dependencies accordingly
    - Improve clean script to clear Watchman cache

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -2,25 +2,18 @@ const {getDefaultConfig} = require("expo/metro-config");
 const {withUniwindConfig} = require("uniwind/metro");
 
 const defaultConfig = getDefaultConfig(__dirname);
-const {assetExts, sourceExts} = defaultConfig.resolver;
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {
-    transformer: {
-        babelTransformerPath: require.resolve("react-native-svg-transformer"),
-    },
-    resolver: {
-        assetExts: assetExts.filter((ext) => ext !== "svg"),
-        sourceExts: [...sourceExts, "svg"],
-    },
-}
+defaultConfig.transformer = {
+    ...defaultConfig.transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
+defaultConfig.resolver = {
+    ...defaultConfig.resolver,
+    assetExts: defaultConfig.resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...defaultConfig.resolver.sourceExts, "svg"],
+};
 
-module.exports = withUniwindConfig(config, {
+module.exports = withUniwindConfig(defaultConfig, {
     // relative path to your global.css file (from previous step)
     cssEntryFile: "./global.css",
     // (optional) path where we gonna auto-generate typings
